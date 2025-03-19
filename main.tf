928447a6bcd34eef9ab4fbb088ec3c7abd22bfbf
anoop <anoopsabu@live.com>
2025-03-19 10:03:07 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.23.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "5656a66f-c902-4aa6-a499-62957d02b7ed"
+}
+
+# Create Resource Group
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "aks-resource-group"
+  location = "eastus"
+}
+
+# Create Virtual Network
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = "aks-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+}
+
+# Create Subnet
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.aks_rg.name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    auto_scaling_enabled = true
+    min_count = 2
+    max_count = 5 
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.example.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+  sensitive = true
+}
